1c8ad0961776cc9397dce899a8fd6020ae3d5104
Tofik <Tofkazlo@gmail.com>
2025-06-23 18:51:32 +0300

    refactor: убрана установка webhook из кода

--- main.py
+++ main.py
@@ -5,27 +5,14 @@ from telegram import Bot, Update
 from dotenv import load_dotenv
 from service import get_plant_data, format_plant_info
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-
 # Загрузка переменных
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = Bot(token=TOKEN)
 app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup():
-    if WEBHOOK_URL:
-        webhook_url = f"{WEBHOOK_URL}/webhook"
-        result = await bot.set_webhook(webhook_url)
-        logging.info(f"✅ Webhook установлен: {result} → {webhook_url}")
-    else:
-        logging.warning("⚠️ Переменная WEBHOOK_URL не задана.")
-
+logging.basicConfig(level=logging.INFO)
 
 @app.post("/webhook")
 async def telegram_webhook(req: Request):
@@ -43,19 +30,15 @@ async def telegram_webhook(req: Request):
     logging.info(f"🗣 Пользователь: {chat_id} → {text}")
 
     plant = get_plant_data(text)
-
     if plant:
         reply = format_plant_info(plant)
         image = plant.get("image")
 
         if image:
-            logging.info("🖼 Отправка карточки с фото.")
             bot.send_photo(chat_id=chat_id, photo=image, caption=reply, parse_mode="HTML")
         else:
-            logging.info("📨 Отправка карточки без фото.")
             bot.send_message(chat_id=chat_id, text=reply, parse_mode="HTML")
     else:
-        logging.info("❌ Растение не найдено.")
         bot.send_message(chat_id=chat_id, text="❌ Растение не найдено.")
 
     return {"ok": True}
