d05ad6982cb3ce262f0cf1b1fcc3e49c24a62a73
VeryHappyEpta <Tofkazlo@Gmai.com>
2025-06-24 18:21:40 +0300

    добавлена кнопка инфо

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import json
 import logging
 from fastapi import FastAPI, Request
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup
 from service import get_plant_data, format_plant_info
 
 logging.basicConfig(level=logging.INFO)
@@ -11,8 +11,13 @@ logger = logging.getLogger(__name__)
 TELEGRAM_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TELEGRAM_TOKEN)
 
-app = FastAPI()
+info_keyboard = ReplyKeyboardMarkup(
+    keyboard=[["ℹ️ Инфо"]],
+    resize_keyboard=True,
+    one_time_keyboard=False
+)
 
+app = FastAPI()
 
 @app.post("/webhook")
 async def telegram_webhook(request: Request):
@@ -29,6 +34,14 @@ async def telegram_webhook(request: Request):
 
         logger.info(f"🗣 Пользователь: {chat_id} → {text}")
 
+        if text.lower() in ["инфо", "ℹ️ инфо"]:
+            bot.send_message(
+                chat_id=chat_id,
+                text="🌿 Этот бот помогает ухаживать за растениями.\nНапиши название растения — получишь инструкцию, фото и факты.",
+                reply_markup=info_keyboard
+            )
+            return {"status": "ok"}
+
         plant = get_plant_data(text)
 
         if plant:
@@ -37,11 +50,11 @@ async def telegram_webhook(request: Request):
             image_path = f"images/{plant.get('image')}"
             if plant.get("image") and os.path.exists(image_path):
                 with open(image_path, "rb") as image:
-                    bot.send_photo(chat_id=chat_id, photo=image, caption=reply, parse_mode="HTML")
+                    bot.send_photo(chat_id=chat_id, photo=image, caption=reply, parse_mode="HTML", reply_markup=info_keyboard)
             else:
-                bot.send_message(chat_id=chat_id, text=reply, parse_mode="HTML")
+                bot.send_message(chat_id=chat_id, text=reply, parse_mode="HTML", reply_markup=info_keyboard)
         else:
-            bot.send_message(chat_id=chat_id, text="❗ Растение не найдено. Попробуйте другое название.")
+            bot.send_message(chat_id=chat_id, text="❗ Растение не найдено. Попробуйте другое название.", reply_markup=info_keyboard)
     except Exception as e:
         logger.error("❌ Ошибка в обработке сообщения", exc_info=e)
 
